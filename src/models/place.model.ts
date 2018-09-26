6a5ba33603d04c2dda5fd46a012e405a8eee84a1
Németh Sándor <n.sandor93@gmail.com>
2018-09-26 16:14:16 +0200

    google maps előtt

---
+++ src/models/place.model.ts
@@ -0,0 +1,10 @@
+import {LocationModel} from "./location.model";
+
+export class PlaceModel {
+  constructor(
+    public title: string,
+    public description: string,
+    public location: LocationModel,
+    public imgPath: string
+  ){}
+}
